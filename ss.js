67ed547a33fe4af96c0294a34b9e4edb6efc42b4
asd <you@sa.ya>
2024-04-17 19:50:50 +0800

    gg

--- ss.js
+++ ss.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $.post('https://tt.networkreverse.com/tapi', {vid:'7358399196233288966',app:'tiktok'}, function(data) {
+  $.post('https://tt.networkreverse.com/tapi', {vid:'7357960040021593349',app:'tiktok'}, function(data) {
   console.log(data);
   });
 });
