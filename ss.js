20cfd567a3ac12eb51310b4b57858ef5cbac6879
asd <you@sa.ya>
2024-04-19 01:50:02 +0800

    asd

--- ss.js
+++ ss.js
@@ -2,7 +2,7 @@ var im;
 function gg(s,t){
   // console.log(im.format[t].format_note);
   if(s<1){
-    $('#test').html("<video preload='metadata' id='video' loop muted controls src='"+im.format[t-1].url+"' onloadedmetadata='gg(this.videoHeight,"+(t-1)+")'></video>");
+    $('#test').html("<video preload='metadata' id='video' loop muted controls src='"+im.format[t-1].url+"' onerror='gg(this.videoHeight,"+(t-1)+")' onloadedmetadata='gg(this.videoHeight,"+(t-1)+")'></video>");
     // console.log(s,t);
   }
 }
@@ -16,7 +16,7 @@ $(document).ready(function(){
       im.format.forEach(el=>{
         $('#link').append("<a href='"+el.url+"' download='"+im.uploader+"-"+im.id+"'>"+el.resolution+"-"+el.filesize+"-"+el.vcodec+"-"+el.format_note+"</a>");
       })
-      $('#test').html("<video preload='metadata' id='video' loop muted controls src='"+im.format[im.format.length-1].url+"' onloadedmetadata='gg(this.videoHeight,im.format.length-1)'></video>");
+      $('#test').html("<video preload='metadata' id='video' loop muted controls src='"+im.format[im.format.length-1].url+"' onerror='gg(this.videoHeight,im.format.length-1)' onloadedmetadata='gg(this.videoHeight,im.format.length-1)'></video>");
     });
   });
 });
