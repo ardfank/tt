dfff2dba59b089141fca49120f3393be0ea2aede
asd <you@sa.ya>
2024-04-19 09:13:42 +0800

    asd

--- ss.js
+++ ss.js
@@ -6,7 +6,10 @@ function gg(s,t){
     // console.log(s,t);
   }
 }
-
+function hf(size) {
+    var i = size == 0 ? 0 : Math.floor(Math.log(size) / Math.log(1024));
+    return (size / Math.pow(1024, i)).toFixed(2) * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
+}
 $(document).ready(function(){
   $('#vid').on('change',function(){
     let vid=$(this).val();
@@ -14,7 +17,7 @@ $(document).ready(function(){
       im=data;
     }).done(function(){
       im.format.forEach(el=>{
-        $('#link').prepend("<a href='"+el.url+"' download='"+im.uploader+"-"+im.id+"'>"+el.resolution+"-"+el.filesize+"-"+el.vcodec+"-"+el.format_note+"</a>");
+        $('#link').prepend("<a href='"+el.url+"' note='"+el.format_note+"' download='"+im.uploader+"-"+im.id+"'>"+el.resolution+"-"+hf(el.filesize)+"<br/>("+el.vcodec+")</a>");
       })
       $('#test').html("<video preload='metadata' id='video' loop muted controls src='"+im.format[im.format.length-1].url+"' onerror='gg(this.videoHeight,im.format.length-1)' onloadedmetadata='gg(this.videoHeight,im.format.length-1)'></video>");
     });
